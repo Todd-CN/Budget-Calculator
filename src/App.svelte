4b79efdc8713142b92fbd201e3401b07d44bc3a4
Todd Sutherland <todd.sutherland@cliniciannexus.com>
2021-03-27 12:29:58 -0500

    budget-calculator font-awesome and global css added to starter code

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,8 @@
 
 <main>
   <h1>Hello Starter Code! {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+
+  <i class="fas fa-home fa-5x" />
 </main>
 
 <style>
