0efc6872d522672cb1ec1b44cd5f9e4ef6d86778
Todd Sutherland <todd.sutherland@cliniciannexus.com>
2021-03-27 13:19:41 -0500

    Example css global vs locally scoped in each component

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script>
   import Navbar from "./Navbar.svelte";
   import Title from "./Title.svelte";
+  import Example from "./Example.svelte";
 </script>
 
 <Navbar />
 <Title title="add expense" />
 <Title title="expense list" />
 <Title />
+<Example />
---
+++ src/Example.svelte
@@ -0,0 +1,11 @@
+<span> Hello from example world! </span>
+<!-- This will override the global scope, and it is only
+scoped to this component, meaning that the css we created here
+won't bleed into other spans we create in other components -->
+
+<style>
+  span {
+    background: red;
+    color: white;
+  }
+</style>
